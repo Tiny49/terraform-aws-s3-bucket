31c8324d8712e2a7ddc9f2a097f5d08cb67a51d6
Seun O <91503321+Tiny49@users.noreply.github.com>
2022-08-30 11:45:59 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -52,6 +52,11 @@ variable "bucket_prefix" {
   default     = null
 }
 
+variable "prefix" {
+  type = string
+  default = null
+}
+
 variable "acl" {
   description = "(Optional) The canned ACL to apply. Conflicts with `grant`"
   type        = string
